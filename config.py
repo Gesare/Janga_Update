fd752dcce08d203b490077bde3a5f8871b62366a
victormongare1 <vicmongz254@gmail.com>
2020-05-12 14:15:41 +0300

    created config options dictionary to run application on diffrent configurations

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ class Config:
   General configuration parent class
   '''
   SOURCE_BASE_URL='https://newsapi.org/v2/sources?apiKey={}'
-  ARTICLE_BASE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
   NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-  SECRET_KEY=os.environ.get('SECRET_KEY')
+  
 class ProdConfig(Config):
   '''
   Production  configuration child class
