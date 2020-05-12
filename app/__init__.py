7000f7d4f308ca0c8cfb8679a84ed7e2fbb29f0c
victormongare1 <vicmongz254@gmail.com>
2020-05-12 14:28:38 +0300

    created app instance to run the program on local server

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_option
+from config import config_options
 
 bootstrap=Bootstrap()
 
---
+++ manage.py
@@ -0,0 +1,15 @@
+from app import create_app
+from flask_script import Manager,Server
+#creating app instance
+
+app = create_app('development')
+manager=Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+  import unittest
+  tests=unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__ == "__main__":
+    manager.run()
