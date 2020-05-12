4bc831cd6bb852cc1c65c710c2b66e0cef1362dc
victormongare1 <vicmongz254@gmail.com>
2020-05-12 14:37:28 +0300

    created articles class to make articles items from the api instances of Article object

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Articles:
+  '''
+  Article class to define article objects
+  '''
+  def __init__(self,author,title,description,url,urlToImage,publishedAt):
+    self.author=author
+    self.title=title
+    self.description=description
+    self.url=url
+    self.urlToImage=urlToImage
+    self.publishedAt=publishedAt
+
