006eec1f09f4b67ddd0acc973a3a74e18d67fbfa
JeromeMberia <jeromemberia@gmail.com>
2020-05-13 11:34:40 +0300

    updating files

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
-
-import os
-
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-#creating app instance
 
-app = create_app(os.environ.get('CONFIG_OPTION'))
+
+#creating app instance
+app = create_app('development')
 manager=Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app, db)
