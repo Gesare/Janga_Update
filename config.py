2a4ba53a8d369edb9d7e49fbff5abe54168e8a85
Gesare <gesareayienda@gmail.com>
2020-05-14 21:08:44 +0300

    Home  design

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ class Config:
   General configuration parent class
   '''
   BASE_URL='https://newsapi.org/v2/everything?q=floods&earthquakes&landslides&volcano-eruption&apiKey={}'
+  CORONA_URL='https://api.covid19api.com/summary'
   API_KEY=os.environ.get('API_KEY')
   SECRET_KEY = os.environ.get("SECRET_KEY")
   CSRF_ENABLED = os.environ.get('CSRF_ENABLED')
-  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jerome:1234@localhost/blog'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gesare:ombati@localhost/janga'
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
@@ -31,7 +32,7 @@ class DevConfig(Config):
   Args:
   Config: The parent configuration class with General configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jerome:1234@localhost/blog'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gesare:ombati@localhost/janga'
   DEBUG = True
 
 class TestingConfig(Config):
---
+++ tests/test_country.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Countries
+class CountryTest(unittest.TestCase):
+  '''
+  test class to test behaviour of the country class
+  '''
+  def setUp(self):
+    '''
+    setup method that will run before every test
+    '''
+    self.new_country=Countries("Kenya",10,10,10)
+  def test_instance(self):
+    '''
+    test to assert whether new_country is an instance of the Countries object
+    '''
+    self.assertTrue(isinstance(self.new_country,Countries))
